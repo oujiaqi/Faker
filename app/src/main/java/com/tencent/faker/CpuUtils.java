dad46ee29259e2859ebcd4f4810bb018cf5464d9
oujiaqi <471884121@qq.com>
2017-11-02 02:01:07 +0800

    init

---
+++ app/src/main/java/com/tencent/faker/CpuUtils.java
@@ -0,0 +1,241 @@
+package com.tencent.faker;
+
+import android.os.Build;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ouou on 2017/11/2.
+ */
+public class CpuUtils {
+    public static final int DEVICEINFO_UNKNOWN = -1;
+
+    public static int getNumberOfCPUCores() {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD_MR1) {
+            // Gingerbread doesn't support giving a single application access to both cores, but a
+            // handful of devices (Atrix 4G and Droid X2 for example) were released with a dual-core
+            // chipset and Gingerbread; that can let an app in the background run without impacting
+            // the foreground application. But for our purposes, it makes them single core.
+            return 1;
+        }
+        int cores;
+        try {
+            cores = new File("/sys/devices/system/cpu/").listFiles(CPU_FILTER).length;
+        } catch (SecurityException e) {
+            cores = DEVICEINFO_UNKNOWN;
+        } catch (NullPointerException e) {
+            cores = DEVICEINFO_UNKNOWN;
+        }
+        return cores;
+    }
+
+    public static String getCPUModel() {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.GINGERBREAD_MR1) {
+            return "";
+        }
+        String model = "";
+        try {
+            FileInputStream stream = new FileInputStream("/proc/cpuinfo");
+            try {
+                String line; // 用来保存每行读取的内容
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                line = reader.readLine(); // 读取第一行
+                while (line != null) { // 如果 line 为空说明读完了
+                    if (line.matches("(?i)Hardware.*")) {
+//                        LogUtil.i("CPU", "cpuinfo: " + line);
+                        System.out.println("cpuinfo:"+line);
+                        model =  line.substring(line.indexOf(":") + 1).trim();
+                    }
+                    line = reader.readLine(); // 读取下一行
+                }
+            } catch (Exception e) {
+//                LogUtil.e("CPU", e.getMessage());
+                System.out.println("CPU: "+e.getMessage());
+            } finally {
+                stream.close();
+            }
+        } catch (SecurityException e) {
+//            Logger.i(TAG, "")
+        } catch (Exception e) {
+//            cores = DEVICEINFO_UNKNOWN;
+        }
+        return model;
+    }
+
+
+    private static final FileFilter CPU_FILTER = new FileFilter() {
+        @Override
+        public boolean accept(File pathname) {
+            String path = pathname.getName();
+            //regex is slow, so checking char by char.
+            if (path.startsWith("cpu") && path.length() > 3) {
+                for (int i = 3; i < path.length(); i++) {
+                    if (path.charAt(i) < '0' || path.charAt(i) > '9') {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+    };
+
+    public static int getCPUMaxFreqKHz() {
+        int maxFreq = DEVICEINFO_UNKNOWN;
+        try {
+            for (int i = 0; i < getNumberOfCPUCores(); i++) {
+                String filename =
+                        "/sys/devices/system/cpu/cpu" + i + "/cpufreq/cpuinfo_max_freq";
+                File cpuInfoMaxFreqFile = new File(filename);
+                if (cpuInfoMaxFreqFile.exists()) {
+                    byte[] buffer = new byte[128];
+                    FileInputStream stream = new FileInputStream(cpuInfoMaxFreqFile);
+                    try {
+                        stream.read(buffer);
+                        int endIndex = 0;
+                        //Trim the first number out of the byte buffer.
+                        while (buffer[endIndex] >= '0' && buffer[endIndex] <= '9'
+                                && endIndex < buffer.length) endIndex++;
+                        String str = new String(buffer, 0, endIndex);
+                        Integer freqBound = Integer.parseInt(str);
+                        if (freqBound > maxFreq) maxFreq = freqBound;
+                    } catch (NumberFormatException e) {
+                        //Fall through and use /proc/cpuinfo.
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        stream.close();
+                    }
+                }
+            }
+            if (maxFreq == DEVICEINFO_UNKNOWN) {
+                FileInputStream stream = new FileInputStream("/proc/cpuinfo");
+                try {
+                    int freqBound = parseFileForValue("cpu MHz", stream);
+                    freqBound *= 1000; //MHz -> kHz
+                    if (freqBound > maxFreq) maxFreq = freqBound;
+                } finally {
+                    stream.close();
+                }
+            }
+        } catch (IOException e) {
+            maxFreq = DEVICEINFO_UNKNOWN; //Fall through and return unknown.
+        }
+        return maxFreq;
+    }
+
+    public static String getCpuCurrentKHZ() {
+        String result = "";
+        try {
+            for (int i = 0; i < getNumberOfCPUCores(); i++) {
+                String filename =
+                        "/sys/devices/system/cpu/cpu" + i + "/cpufreq/scaling_cur_freq";
+                File cpuInfoCurrentFreqFile = new File(filename);
+                if (cpuInfoCurrentFreqFile.exists()) {
+                    byte[] buffer = new byte[128];
+                    FileInputStream stream = new FileInputStream(cpuInfoCurrentFreqFile);
+                    try {
+                        stream.read(buffer);
+                        int endIndex = 0;
+                        //Trim the first number out of the byte buffer.
+                        while (buffer[endIndex] >= '0' && buffer[endIndex] <= '9'
+                                && endIndex < buffer.length) endIndex++;
+                        String str = new String(buffer, 0, endIndex);
+                        Integer freqBound = Integer.parseInt(str);
+                        result = result + freqBound +";";
+                    } catch (NumberFormatException e) {
+                        //Fall through and use /proc/cpuinfo.
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        stream.close();
+                    }
+                }
+            }
+        } catch (IOException e) {
+        }
+        return result;
+    }
+
+    private static int parseFileForValue(String textToMatch, FileInputStream stream) {
+        byte[] buffer = new byte[1024];
+        try {
+            int length = stream.read(buffer);
+            for (int i = 0; i < length; i++) {
+                if (buffer[i] == '\n' || i == 0) {
+                    if (buffer[i] == '\n') i++;
+                    for (int j = i; j < length; j++) {
+                        int textIndex = j - i;
+                        //Text doesn't match query at some point.
+                        if (buffer[j] != textToMatch.charAt(textIndex)) {
+                            break;
+                        }
+                        //Text matches query here.
+                        if (textIndex == textToMatch.length() - 1) {
+                            return extractValue(buffer, j);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            //Ignore any exceptions and fall through to return unknown value.
+        } catch (NumberFormatException e) {
+        }
+        return DEVICEINFO_UNKNOWN;
+    }
+    private static int extractValue(byte[] buffer, int index) {
+        while (index < buffer.length && buffer[index] != '\n') {
+            if (Character.isDigit(buffer[index])) {
+                int start = index;
+                index++;
+                while (index < buffer.length && Character.isDigit(buffer[index])) {
+                    index++;
+                }
+                String str = new String(buffer, 0, start, index - start);
+                return Integer.parseInt(str);
+            }
+            index++;
+        }
+        return DEVICEINFO_UNKNOWN;
+    }
+
+    public  static int getProcessCpuRate() {
+
+        StringBuilder tv = new StringBuilder();
+        int rate = 0;
+
+        try {
+            String Result;
+            Process p;
+            p = Runtime.getRuntime().exec("top -n 1");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((Result = br.readLine()) != null) {
+                if (Result.trim().length() < 1) {
+                    continue;
+                } else {
+                    String[] CPUusr = Result.split("%");
+                    tv.append("USER:" + CPUusr[0] + "\n");
+                    String[] CPUusage = CPUusr[0].split("User");
+                    String[] SYSusage = CPUusr[1].split("System");
+                    tv.append("CPU:" + CPUusage[1].trim() + " length:" + CPUusage[1].trim().length() + "\n");
+                    tv.append("SYS:" + SYSusage[1].trim() + " length:" + SYSusage[1].trim().length() + "\n");
+
+                    rate = Integer.parseInt(CPUusage[1].trim()) + Integer.parseInt(SYSusage[1].trim());
+                    break;
+                }
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(rate + "");
+        return rate;
+    }
+}
